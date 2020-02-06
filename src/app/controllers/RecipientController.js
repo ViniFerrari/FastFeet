139409df07656c77d61051631863c67820fa11c6
Vinicius Ferrari Oliveira <viniferrari1999@gmail.com>
2020-02-05 21:03:56 -0300

    Primeiro commit; Desafio 02

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,92 @@
+// https://skylab.rocketseat.com.br/h/forum/bootcamp/69231aac-e6c5-4664-a722-263a2bfb023d
+// Falta a validação; adicionar as routes; yarn sequelize db:migrate; testes insominia
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+// import User from '../models/User';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number()
+        .required()
+        .integer()
+        .positive(),
+      complement: Yup.string(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists.' });
+    }
+
+    const { id, name, street, state } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      state,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number()
+        .integer()
+        .positive(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip_code: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    let recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exist' });
+    }
+
+    const { name, street, number, state } = req.body;
+
+    if (
+      name === recipient.name &&
+      street === recipient.street &&
+      number === recipient.number
+    ) {
+      return res
+        .status(400)
+        .json({ error: 'Recipient already exist in this address' });
+    }
+
+    recipient = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      state,
+    });
+  }
+}
+
+export default new RecipientController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'dd87d8e0aad462d510efa91204c29c52',
+  expiresIn: '7d',
+};
